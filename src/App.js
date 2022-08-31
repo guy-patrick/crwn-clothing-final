1d24bfaabedb347339082d51136936c63601fa1d
Patrick OUAMBO <patgp04@gmail.com>
2022-08-31 16:26:30 +0100

    update app component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 
+function Shop() {
+  return <h1>Shop route</h1>;
+}
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
